84d1bb7c2874b33f7c36b967a3218d9b76b53099
Davinder Kumar <davindertwal@gmail.com>
2023-07-05 12:51:08 +0530

    addedData

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,20 +20,17 @@ const Home = () => {
           <li className="item">
             <h2> Front-End</h2>
             <span>
-              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
-              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+              ReactJS, Redux, HTML, CSS,NPM, BootStrap, MaterialUI, Yarn,
+              TailwindCSS, StyledComponents
             </span>
           </li>
           <li className="item">
             <h2>Back-End</h2>
-            <span>
-              NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
-              MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
-            </span>
+            <span>NodeJS, ExpressJS, MySQL, MongoDB</span>
           </li>
           <li className="item">
             <h2>Languages</h2>
-            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
+            <span>JavaScript, Java</span>
           </li>
         </ol>
       </div>
